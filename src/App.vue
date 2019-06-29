9557bb2922124bc5cef09e94bdec37a1e41674c1
henry <henryonyemaobi@gmail.com>
2019-06-29 16:55:34 +0100

    Ui cleaned and ready

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,178 @@
 <template>
   <div id="app">
-    nn
+    	<!-- header-section-starts -->	
+	<div class="header" id="header">
+      <div class="header-top">
+        <div class="container">
+          <p class="location"><span class="glyphicon glyphicon-map-marker" aria-hidden="true">
+  
+          </span>29C, Autem quibusdam debitis.</p>
+          <p class="phonenum"><span class="glyphicon glyphicon-earphone" aria-hidden="true">
+            
+          </span><a href='tel:+2348035562231'>+23480 3556 2231</a></p>
+          <div class="social-icons">
+            <ul>
+              <li><a href="https://www.facebook.com/mobyf" target='_blank'><i class="facebook"> </i></a></li>
+              <li><a href="mailto:stevenonyemaobi@gmail.com"><i class="google-plus"> </i></a></li>							
+            </ul>
+          </div>
+          <div class="clearfix"></div>
+        </div>
+      </div>
+      <div class="header-bottom">
+      <div class="logo text-center">
+          <h1><a href="/">Habitart</a></h1>
+        </div>
+  
+          <div class="clearfix"></div>
+        </div>
+  
+    </div>
+    <div class="screen-shots">
+            <!--sreen-gallery-cursual-->
+              <div class="sreen-gallery-cursual">
+                <!-- start content_slider -->
+                     <div id="owl-demo" class="owl-carousel">
+                            <div class="item">
+                              <div class="item-grid">
+                                <div class="item-pic">
+                                  <img src="/static/images/t1.jpg" alt="" />
+                          <div class="item-text text-center">
+                            <p class="s-para">The standard design</p>
+                            <h5 class="s-heading">Lorem Ipsum is simply dummy text printing </h5>
+                          </div>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="item">
+                              <div class="item-grid">
+                                <div class="item-pic">
+                                  <img src="/static/images/t2.jpg" alt="" />
+                          <div class="item-text text-center">
+                            <p class="s-para">The standard design</p>
+                            <h5 class="s-heading">Lorem Ipsum is simply dummy text printing </h5>
+                          </div>
+                                                 </div>
+                                </div>
+                            </div>
+                             <div class="item">
+                              <div class="item-grid">
+                                <div class="item-pic">
+                                  <img src="/static/images/t3.jpg" alt="" />
+                          <div class="item-text text-center">
+                            <p class="s-para">The standard design</p>
+                            <h5 class="s-heading">Lorem Ipsum is simply dummy text printing </h5>
+                          </div>
+                                                 </div>
+                                </div>
+                              </div>
+                           <div class="item">
+                              <div class="item-grid">
+                                <div class="item-pic">
+                                  <img src="/static/images/t4.jpg" alt="" />
+                          <div class="item-text text-center">
+                            <p class="s-para">The standard design</p>
+                            <h5 class="s-heading">Lorem Ipsum is simply dummy text printing </h5>
+                          </div>
+                                                 </div>
+                                </div>
+                              </div>
+                             <div class="item">
+                              <div class="item-grid">
+                                <div class="item-pic">
+                                  <img src="/static/images/t1.jpg" alt="" />
+                          <div class="item-text text-center">
+                            <p class="s-para">The standard design</p>
+                            <h5 class="s-heading">Lorem Ipsum is simply dummy text printing </h5>
+                          </div>
+                                                 </div>
+                                </div>
+                              </div>
+                             <div class="item">
+                              <div class="item-grid">
+                                <div class="item-pic">
+                                  <img src="/static/images/t2.jpg" alt="" />
+                          <div class="item-text text-center">
+                            <p class="s-para">The standard design</p>
+                            <h5 class="s-heading">Lorem Ipsum is simply dummy text printing </h5>
+                          </div>
+                                                 </div>
+                                </div>
+                              </div>
+                           <div class="item">
+                              <div class="item-grid">
+                                <div class="item-pic">
+                                  <img src="/static/images/t3.jpg" alt="" />
+                          <div class="item-text text-center">
+                            <p class="s-para">The standard design</p>
+                            <h5 class="s-heading">Lorem Ipsum is simply dummy text printing </h5>
+                          </div>
+                                                 </div>
+                                </div>
+                              </div>
+                             <div class="item">
+                              <div class="item-grid">
+                                <div class="item-pic">
+                                  <img src="/static/images/t4.jpg" alt="" />
+                          <div class="item-text text-center">
+                            <p class="s-para">The standard design</p>
+                            <h5 class="s-heading">Lorem Ipsum is simply dummy text printing </h5>
+                          </div>
+                                                 </div>
+                                </div>
+                              </div>
+                            </div>
+                        </div>
+              <!--//sreen-gallery-cursual-->
+            </div>
+          
+    <!----> 
+  <div class="gallery">
+      <div class="container">
+        <h2 class="heading text-center">My Designs</h2>
+        <div class="gallery-bottom">
+            <div class="col-md-3 gallery-grid">
+              <a class="example-image-link" href="/static/images/g1.jpg" data-lightbox="example-set" data-title="Click the right half of the image to move forward."><img class="example-image" src="/static/images/g1.jpg" alt=""/></a>
+            </div>
+            <div class="col-md-3 gallery-grid">
+              <a class="example-image-link" href="/static/images/g2.jpg" data-lightbox="example-set" data-title="Click the right half of the image to move forward."><img class="example-image" src="/static/images/g2.jpg" alt=""/></a>
+            </div>
+            <div class="col-md-3 gallery-grid">
+              <a class="example-image-link" href="/static/images/g3.jpg" data-lightbox="example-set" data-title="Click the right half of the image to move forward."><img class="example-image" src="/static/images/g3.jpg" alt=""/></a>
+            </div>
+            <div class="col-md-3 gallery-grid">
+              <a class="example-image-link" href="/static/images/g4.jpg" data-lightbox="example-set" data-title="Click the right half of the image to move forward."><img class="example-image" src="/static/images/g4.jpg" alt=""/></a>
+            </div>
+            <div class="col-md-3 gallery-grid">
+              <a class="example-image-link" href="/static/images/g5.jpg" data-lightbox="example-set" data-title="Click the right half of the image to move forward."><img class="example-image" src="/static/images/g5.jpg" alt=""/></a>
+            </div>
+            <div class="clearfix"></div>
+          </div>
+          
+       </div>
+     </div>
+  
+  <!----> 
+  
+      <!-- footer-section -->
+    <div class="footer">
+    
+      <div class="footer-bottom">
+        <div class="container">
+          <div class="copyrights col-md-10">
+            <!--<p>© 2015 Interior Design. All Rights Reserved | Design by
+                 <a href="http://w3layouts.com">  W3layouts</a></p>	-->
+          <p>© 2019. All Rights Reserved | Architect Steve Onyemaobi</p>	   				
+          </div>
+          <div class="go-top col-md-2">
+            <a href="#header" class="scroll"><span class="glyphicon glyphicon-chevron-up"></span></a>
+          </div>
+          <div class="clearfix"></div>
+        </div>
+      </div>
+    </div>
+    <!-- footer-section -->
+  <!----> 
   </div>
 </template>
 
